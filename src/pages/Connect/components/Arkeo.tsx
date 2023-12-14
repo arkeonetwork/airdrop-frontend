04981aad8133bea39b36802b41ef20ecdce667f3
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-12-14 12:32:30 -0700

    Integrate arkeo with keplr

--- src/pages/Connect/components/Arkeo.tsx
+++ src/pages/Connect/components/Arkeo.tsx
@@ -3,7 +3,7 @@ import { Button, Box, Text, Image, Flex } from '@chakra-ui/react';
 import ArkeoLogo from '@assets/arkeo-symbol-grey.svg';
 import { useConnect } from '../ConnectContext';
 import { ConnectedAccount } from './ConnectedAccount';
-import { bech32, bech32m } from 'bech32';
+import { useChain } from '@cosmos-kit/react';
 
 type Props = {};
 
@@ -12,28 +12,29 @@ export const Arkeo: React.FC<Props> = ({}) => {
     state: { step, arkeoAccount, cosmosAccount },
     dispatch,
   } = useConnect();
+  const { status, username, address, message, connect, disconnect, openView, sign, isWalletConnected } = useChain('arkeonetworktestnet');
+
+  useEffect(() => {
+    dispatch({ type: 'SET_ARKEO_ACCOUNT', payload: address });
+  }, [address]);
 
   const handleClick = () => {
     if (arkeoAccount) {
       dispatch({ type: 'SET_STEP', payload: step + 1 });
+    } else {
+      openView();
     }
   };
 
   const renderWallet = () => {
     if (arkeoAccount) {
-      return <ConnectedAccount width="100%" amount="100" account={arkeoAccount} disconnect={() => {}} />;
+      return <ConnectedAccount width="100%" amount="100" account={arkeoAccount} disconnect={disconnect} />;
     }
     return <Image w="150px" h="150px" src={ArkeoLogo} />;
   };
 
-  useEffect(() => {
-    if (cosmosAccount) {
-      const { prefix, words } = bech32.decode(cosmosAccount);
-      const address = bech32.encode('arkeo', words);
-      dispatch({ type: 'SET_ARKEO_ACCOUNT', payload: address });
-    }
-  }, [cosmosAccount]);
-  
+
+
   return (
     <>
       <Flex flexDir="column" flex="1 0 0" gap="42px" textAlign="center" alignItems="center" justifyContent="space-between">
